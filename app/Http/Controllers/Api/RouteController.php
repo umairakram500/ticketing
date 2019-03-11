ee3a78a1bdb3c745f4fe26eee081d77cf908504b
Umair Akram <umairakram500@gmail.com>
2019-03-11 15:08:00 +0500

    update

--- app/Http/Controllers/Api/RouteController.php
+++ app/Http/Controllers/Api/RouteController.php
@@ -27,26 +27,15 @@ class RouteController extends Controller
         $data['to'] = $route->to_terminal_id;
         /*$data['stops'] = Stop::select('terminal_id')->with('terminal')
                     ->where('route_id', $route->id)->get()->pluck('terminal.title', 'terminal_id')->toArray();*/
-        $stops = $route->stops()->with('terminal')->get();
-        $fares = $route->fares()
-            ->select(['from_terminal_id as from_stop', 'to_terminal_id as to_stop', 'fare','luxury_id'])
+        $stops = $route->stops()->with('terminal')->get()->pluck('terminal.title', 'id');
+        $data['fares'] = $route->fares()
+            ->select(['from_terminal_id as from_stop', 'to_terminal_id as to_stop', 'fare', 'luxury_id'])
             ->get()->toArray();
-        $nfares = array();
-        if(count($fares)){
-            foreach($fares as $fare){
-                $far = $fare;
-                unset($far['luxury_id']);
-                $nfares[$fare['luxury_id']][] = $far;
-            }
-            foreach($nfares as $id => $nfare){
-                $data['fares'][] = ['luxury_id'=>$id, 'fares'=>$nfare];
-            }
-        }
 
         $data['schedules'] = $route->schedules()->select(['id', 'depart_time as title', 'luxury_type as luxury_id'])->get()->toArray();
 
-        foreach($stops as $stop)
-            $data['stops'][] = array('id'=>$stop->id, 'title'=>$stop->terminal->title);
+//        foreach($stops as $stop)
+//            $data['stops'][] = array('id'=>$stop->id, 'title'=>$stop->terminal->title);
 
         // dd($data);
 
