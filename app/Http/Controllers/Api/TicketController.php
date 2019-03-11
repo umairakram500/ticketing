b756808e8258dabcfef2d998bd73e312cc6b6856
Umair Akram <umairakram500@gmail.com>
2019-03-11 12:34:07 +0500

    upeates

--- app/Http/Controllers/Api/TicketController.php
+++ app/Http/Controllers/Api/TicketController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Terminal;
+use App\Models\Ticket\Ticket;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +11,23 @@ class TicketController extends Controller
 {
     public function store(Request $req)
     {
-        return response($req->all());
+        //return response($req->all());
+
+        $ticket = new Ticket();
+
+        $ticket->route_id = $req->route_id;
+        $ticket->schedule_id = $req->schedule_id;
+        $ticket->from_stop = $req->from_stop;
+        $ticket->to_stop = $req->to_stop;
+        $ticket->total_fare = $req->fare;
+        $ticket->total_seats = $req->psgs;
+        $ticket->from_city_id = Terminal::find($req->from_stop)->city_id;
+        $ticket->to_city_id = Terminal::find($req->to_stop)->city_id;
+        $ticket->btype_id = 6;
+
+        $ticket->save();
+
+        return response($ticket);
+
     }
 }
